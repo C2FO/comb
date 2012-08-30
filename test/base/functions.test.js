c98c3440f31eec7497514cbd4f18f9828f3bf163
Doug Martin <doug@dougamartin.com>
2012-08-30 12:35:24 -0500

    new function features
    * hitch and partial now accept property names to access
    * New method applyFirst
    * code clean up

--- test/base/functions.test.js
+++ test/base/functions.test.js
@@ -7,7 +7,7 @@ var it = require('it'),
     Broadcaster = comb;
 
 
-it.describe("comb/base/functions.js", function (it) {
+it.describe("comb/base/functions.js",function (it) {
 
 
     it.should("test if something is a function", function () {
@@ -43,7 +43,24 @@ it.describe("comb/base/functions.js", function (it) {
         it.should("throw an error with an invalid function", function () {
             assert.throws(function () {
                 comb.hitch(comb, "someFunction");
-            })
+            });
+
+            assert.throws(function () {
+                comb.hitch(comb, new Date());
+            });
+        });
+
+        it.should("work with non function properties also", function () {
+            var val = [], accessor = comb.hitch(val, "length");
+            val.push(1);
+            assert.equal(accessor(), 1);
+
+            val.push(3);
+            assert.equal(accessor(), 2);
+
+            val.push(4);
+            assert.equal(accessor(), 3);
+
         });
 
 
@@ -63,7 +80,24 @@ it.describe("comb/base/functions.js", function (it) {
         it.should("throw an error with an invalid function", function () {
             assert.throws(function () {
                 comb.hitchIgnore(comb, "someFunction");
-            })
+            });
+
+            assert.throws(function () {
+                comb.hitchIgnore(comb, new Date());
+            });
+        });
+
+        it.should("work with non function properties also", function () {
+            var val = [], accessor = comb.hitchIgnore(val, "length");
+            val.push(1);
+            assert.equal(accessor(), 1);
+
+            val.push(3);
+            assert.equal(accessor(), 2);
+
+            val.push(4);
+            assert.equal(accessor(), 3);
+
         });
     });
 
@@ -97,7 +131,7 @@ it.describe("comb/base/functions.js", function (it) {
         it.should("throw an error with an invalid function", function () {
             assert.throws(function () {
                 comb.bindIgnore(comb, "someFunction");
-            })
+            });
         });
     });
 
@@ -110,6 +144,17 @@ it.describe("comb/base/functions.js", function (it) {
             c.call({test:true}, true);
         });
 
+        it.should("should work with strings", function () {
+            var c = comb.partial("test"), c2 = comb.partial("test2");
+            assert.equal(c.call({test:true}), true);
+            assert.equal(c2.call({
+                test2:function () {
+                    return true;
+                }
+            }), true);
+
+        });
+
         it.should("curry extra parameters", function () {
             var c = comb.partial(function (hello, world) {
                 assert.equal(hello, "hello");
@@ -120,8 +165,55 @@ it.describe("comb/base/functions.js", function (it) {
 
         it.should("throw an error with an invalid function", function () {
             assert.throws(function () {
-                comb.partial(comb, "someFunction");
-            })
+                comb.partial(comb);
+            });
+
+            assert.throws(function () {
+                comb.partial(1);
+            });
+        });
+    });
+
+    it.describe("#applyFirst", function (it) {
+        it.should("execute in the right scope", function (next) {
+            var c = comb.applyFirst(function () {
+                assert.isTrue(this.test);
+                next();
+            });
+            c({test:true});
+        });
+
+        it.should("curry arguments", function (next) {
+            var c = comb.applyFirst(function (testStr, testStr2) {
+                assert.isTrue(this.test);
+                assert.equal(testStr, "HELLO");
+                assert.equal(testStr2, "test");
+                next();
+            }, "HELLO");
+            c({test:true}, "test");
+        });
+
+        it.should("throw an error with an invalid function", function () {
+            assert.throws(function () {
+                comb.applyFirst(1);
+            });
+
+            assert.throws(function () {
+                comb.applyFirst(new Date());
+            });
+        });
+
+        it.should("work with non function properties also", function () {
+            var val = [], push = comb.applyFirst("push"), length = comb.applyFirst("length");
+            push(val, 1);
+            assert.equal(length(val), 1);
+
+            push(val, 3);
+            assert.equal(length(val), 2);
+
+            push(val, 4);
+            assert.equal(length(val), 3);
+
         });
     });
 
